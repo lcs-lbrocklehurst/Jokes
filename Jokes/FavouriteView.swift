e0abcfbe322cce90dbc96fa9c0c432190eeee52c
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-04-18 09:17:56 -0400

    Started to add view to see the list of saved / favourite jokes.

---
+++ Jokes/FavouriteView.swift
@@ -0,0 +1,20 @@
+//
+//  FavouriteView.swift
+//  Jokes
+//
+//  Created by Lewis Brocklehurst on 2023-04-18.
+//
+
+import SwiftUI
+
+struct FavouriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouriteView()
+    }
+}
